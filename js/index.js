e894899494d3384c4b6c189795e73db41ad6fbed
Heather Nye <heatherdnye@gmail.com>
2019-10-06 20:00:04 -0500

    focus

--- js/index.js
+++ js/index.js
@@ -2,31 +2,31 @@
 
 /*--button manipulation--*/
 
-let count = 0;
+// let count = 0;
 
-let highLt = document.querySelectorAll('btn');
-console.log(highLt);
-
-
-while (count < highLt.length) {
+// let highLt = document.querySelectorAll('btn');
+// console.log(highLt);
 
 
-hightLt[count].addEventListener('mouseover', (e) => {
+// while (count < highLt.length) {
+
+
+// hightLt[count].addEventListener('mouseover', (e) => {
     
-    e.target.style.backgroundcolor = 'red' ;
+//     e.target.style.backgroundcolor = 'red' ;
 
-    setTimeout(() => {
-        e.target.style.color = '';
-    }, 100);
+//     setTimeout(() => {
+//         e.target.style.color = '';
+//     }, 100);
 
- }, false);
+//  }, false);
 
- highLt[count].addEventListenter('click', (e) => {
+//  highLt[count].addEventListenter('click', (e) => {
 
-    e.target.style.visibility = 'hidden';
- } );
+//     e.target.style.visibility = 'hidden';
+//  } );
  
-}
+// }
 
 /*--keydown--*/
 
@@ -44,10 +44,23 @@ console.log(para);
 
 
 
+/*--focus event--*/
+
+let nav = document.querySelector('.nav');
+console.log(nav);
+
+nav.addEventListener('focusin', (e) => {
+  e.target.style.background = 'grey';    
+});
+
+nav.addEventListener('focusout', (e) => {
+  e.target.style.background = '';    
+
+}, true);
 
 
 
-
+/**--resize event--*/
 
 
 
